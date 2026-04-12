07d5296ddd45144b815cda42705c15cb0fcfeef1
ST10480829-BandileMthabela <bobmthabela@gmail.com>
2026-04-12 22:07:16 +0200

    user input scanner with code attribution

--- src/main/java/com/mycompany/chat_app_prog5121_a1/Chat_App_PROG5121_A1.java
+++ src/main/java/com/mycompany/chat_app_prog5121_a1/Chat_App_PROG5121_A1.java
@@ -6,5 +6,22 @@ public class Chat_App_PROG5121_A1 {
 
     public static void main(String[] args) {
         
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Please enter username:");
+        String username = input.nextLine();
+        
+        System.out.println("Please enter password.");
+        String password = input.nextLine();
     }
 }
+
+/*
+CODE ATTRIBUTION
+
+Author: Geeks for Geeks
+Title: Scanner Class in Java
+Date Published: 2025
+Link: https://www.geeksforgeeks.org/java/scanner-class-in-java/
+Date accessed: 12/04/2026
+*/
