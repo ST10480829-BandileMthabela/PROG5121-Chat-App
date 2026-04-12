fa62ec65a1f76852a7b7800a538a0ca381553710
ST10480829-BandileMthabela <bobmthabela@gmail.com>
2026-04-12 21:23:51 +0200

    Initial Commit
    Added file to Git Hud for the first time. The main file has been created and partially coded.

---
+++ src/main/java/com/mycompany/chat_app_prog5121_a1/Chat_App_PROG5121_A1.java
@@ -0,0 +1,10 @@
+//Bandile Mthabela ST10480829
+package com.mycompany.chat_app_prog5121_a1;
+import java.util.Scanner;
+
+public class Chat_App_PROG5121_A1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
