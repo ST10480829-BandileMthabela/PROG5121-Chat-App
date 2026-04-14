9978de8ffecc96c5c721df0e67b340edd5e1b529
ST10480829-BandileMthabela <bobmthabela@gmail.com>
2026-04-14 10:04:21 +0200

    code attribution in class
    private, getter, setter

--- src/main/java/com/mycompany/chat_app_prog5121_a1/Chat_App_System.java
+++ src/main/java/com/mycompany/chat_app_prog5121_a1/Chat_App_System.java
@@ -5,4 +5,34 @@ public class Chat_App_System {
 private String username;
 private String password;
 private String cellNumber;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCellNumber() {
+        return cellNumber;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
+/*
+CODE ATTRIBUTION
+
+Author: Geeks for Geeks
+Title: Private Access Modifier
+Date Published: 2026
+Link: https://www.geeksforgeeks.org/java/access-modifiers-java/
+Date accessed: 14/04/2026
+*/
