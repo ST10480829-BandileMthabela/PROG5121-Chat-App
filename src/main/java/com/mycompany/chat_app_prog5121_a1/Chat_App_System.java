2835360917612bf76965fbd3bbf15f087929b8a2
ST10480829-BandileMthabela <bobmthabela@gmail.com>
2026-04-14 14:07:45 +0200

    getter and setter fixed

--- src/main/java/com/mycompany/chat_app_prog5121_a1/Chat_App_System.java
+++ src/main/java/com/mycompany/chat_app_prog5121_a1/Chat_App_System.java
@@ -1,38 +1,61 @@
+//Bandile Mthabela ST10480829
 package com.mycompany.chat_app_prog5121_a1;
 
 public class Chat_App_System {
+ 
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;    
 
-private String username;
-private String password;
-private String cellNumber;
-
-    public String getUsername() {
+    //Getter method to retrieve the username
+    public String getusername() {
         return username;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public String getCellNumber() {
-        return cellNumber;
-    }
-
-    public void setUsername(String username) {
+    //setter method to retrieve the username
+    public void setusername(String username) {
         this.username = username;
     }
 
+    public String getpassword() {
+        return password;
+    }
+    
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
     
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+     public String getLastName() {
+        return firstName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
     //conditions fro the username.
+/*
     public boolean checkUsername(String username)
     {
     return username.contains("_") && username.length()==5;
     }
+    
+    public boolean checkPassword(String password)
+    {
+    return password.length()=8 && password.startsWith("BOB") && password
+    }
+*/
 
-}
 /*
 CODE ATTRIBUTION
 
